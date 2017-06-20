16edd61c14cc3ff58392b44ba37ba6e7b2038a45
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-20 15:29:37 -0400

    Add switch and default route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
@@ -12,6 +13,9 @@ class App extends Component {
             <h1>API Party</h1>
           </div>
         </div>
+        <Switch>
+          <Route render={() => <p>To get started, click one of the links above</p>} />
+        </Switch>
       </div>
     );
   }
